6ec71d293edbbf5bbda6aedfe39c1e42bb032259
AmitKumar8586 <jai06946@gmail.com>
2026-05-25 12:19:27 +0530

    edit the app

--- vite.config.js
+++ vite.config.js
@@ -6,5 +6,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/02_Todo/'
+  base: '/to-do-app/'
 })
